d8aa003e512df3af3d70ede4d51cf7308a80dee8
Oshada Rodney <84395086+OVRupesinghe@users.noreply.github.com>
2022-09-06 08:34:53 +0530

    Update Q1.scala

--- Q1.scala
+++ Q1.scala
@@ -1,19 +1,27 @@
-package Assignment6
+object tute6 extends App{
 
-object Q1 extends App{
-  
-  val alphabet = "abcdefghijklmnopqrstuvwxyz" 
-  //alphabet is in lower case therefore all letters 
+  val alphabet = "abcdefghijklmnopqrstuvwxyz"
+  //alphabet is in lower case therefore all letters
   //in the string should be converted to lower case before calculating new index
-  
-  val E = (c:Char, key:Int, a:String)=>a((a.indexOf(c.toLower)+key)%a.size) //Encryption function
-  val D = (c:Char, key:Int, a:String)=>a((a.indexOf(c.toLower)-key)%a.size) //Decryption function
-  
-  val cipher =(algo:(Char,Int,String)=>Char,s:String,key:Int,a:String)=>s.map(algo(_,key,a))
-  
-  val ct = cipher(E,"s",5,alphabet) //should print as x since s,t,u,v,w,[x]
-  val pt = cipher(D,ct,5,alphabet) //should again print s since decyphering cyphered letter
-  
+
+  val E = (c: Char, key: Int, a: String) => a((a.indexOf(c.toLower) + key) % a.length) //Encryption function
+  val D = (c: Char, key: Int, a: String) => a(((a.indexOf(c.toLower) - key) % a.length).abs) //Decryption function
+
+  val cipher = (algo: (Char, Int, String) => Char, s: String, key: Int, a: String) => s.map(algo(_, key, a))
+
+  val ct = cipher(E, "s", 5, alphabet)
+  val pt = cipher(D, ct, 5, alphabet)
+  val ct2 = cipher(E, "abcd", 5, alphabet)
+  val pt2 = cipher(D, ct2, 5, alphabet)
+  val ct3 = cipher(E, "z", 5, alphabet)
+  val pt3 = cipher(D, ct3, 5, alphabet)
+  println("Cyphering and decyphering a single letter:")
   println(ct)
-  println(pt)  
+  println(pt)
+  println("Cyphering and decyphering a string:")
+  println(ct2)
+  println(pt2)
+  println("Cyphering and decyphering a letter at the edge of the alphabet:")
+  println(ct3)
+  println(pt3)
 }
